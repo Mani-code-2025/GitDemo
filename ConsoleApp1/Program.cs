d76c9e24a9c85be07df141e7e52924986d752c86
Mani <mani.s@isteer.com>
2025-02-12 12:51:11 +0530

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Git Demo with .NET!");
+            Console.WriteLine("This is a simple .NET console application.");
+        }
+    }
+}
